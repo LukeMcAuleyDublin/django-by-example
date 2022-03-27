42f9f9a42623be71f06c59722679a68ade574fba
Luke McAuley <lukemcauley7@gmail.com>
2022-03-27 23:19:01 +0100

    Work done on first project

--- chapter-one/mysite/blog/views.py
+++ chapter-one/mysite/blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        
+    return render(request, 'blog/post/list.html', {'page':page , 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -15,3 +25,4 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+
