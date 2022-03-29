fd29ade84662a0e0b6ebe7c60a3b65bc0c5c4160
Luke McAuley <lukemcauley7@gmail.com>
2022-03-29 18:00:09 +0100

    Added email form

--- chapter-one/mysite/blog/views.py
+++ chapter-one/mysite/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
+from .forms import EmailPostForm
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_list(request):
     object_list = Post.published.all()
@@ -15,6 +24,7 @@ def post_list(request):
         
     return render(request, 'blog/post/list.html', {'page':page , 'posts': posts})
 
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
@@ -25,4 +35,18 @@ def post_detail(request, year, month, day, post):
         publish__day=day
     )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ... send email from here
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {
+            'post':post, 
+            'form':form
+        })
 
